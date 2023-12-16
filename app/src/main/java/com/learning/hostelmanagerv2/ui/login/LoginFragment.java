2d629c30fb0a4e176805fc586b85bb725101f7f1
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-12-16 18:52:51 +0600

    validation added on user input

--- app/src/main/java/com/learning/hostelmanagerv2/ui/login/LoginFragment.java
+++ app/src/main/java/com/learning/hostelmanagerv2/ui/login/LoginFragment.java
@@ -6,6 +6,7 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -32,6 +33,15 @@ public class LoginFragment extends Fragment {
         // Add TextWatcher for password EditText
         binding.password.addTextChangedListener(textWatcher);
 
+        binding.login.setOnClickListener(v -> {
+            String username = binding.userphone.getText().toString().trim();
+            String password = binding.password.getText().toString().trim();
+
+            Toast.makeText(getContext(), "" + username + password, Toast.LENGTH_SHORT).show();
+
+        });
+
+
         return view;
     }
 
@@ -44,11 +54,36 @@ public class LoginFragment extends Fragment {
 
         @Override
         public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            // Check if both usephone and password are not empty and have length greater than 5
+            // Check if both userphone and password are not empty and have length greater than 5
+            //aslo check if userphone is valid bangladeshi phone number
+            boolean isPhoneValid = false;
             String username = binding.userphone.getText().toString().trim();
             String password = binding.password.getText().toString().trim();
+            if (username.startsWith("017") || username.startsWith("018") || username.startsWith("019") || username.startsWith("016") || username.startsWith("015") || username.startsWith("013") || username.startsWith("014")) {
 
-            boolean isValid = !username.isEmpty() && !password.isEmpty() && username.length() == 11 && password.length() > 5;
+                if (username.length() == 11) {
+
+                    //have to do something here
+//                    binding.errormsg.setText("Valid");
+//                    binding.errormsg.setVisibility(View.VISIBLE);
+
+
+                    isPhoneValid = true;
+
+                } else {
+                    binding.errormsg.setText("Invalid Phone Number");
+                    isPhoneValid = false;
+                    binding.errormsg.setVisibility(View.VISIBLE);
+                }
+
+            } else {
+                binding.errormsg.setText("Invalid Phone Number");
+                isPhoneValid = false;
+                binding.errormsg.setVisibility(View.VISIBLE);
+            }
+
+
+            boolean isValid = !username.isEmpty() && !password.isEmpty() && username.length() == 11 && password.length() > 5 && isPhoneValid == true;
 
             // Enable or disable the sign-up button based on the validation result
             binding.login.setEnabled(isValid);
